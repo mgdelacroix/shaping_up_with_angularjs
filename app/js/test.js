db902e1cbd786cd1a6e03e40f1e41fa98b21b4ba
Miguel de la Cruz <mgdelacroix@gmail.com>
2014-09-16 19:41:04 +0200

    Initial commit

---
+++ app/js/test.js
@@ -0,0 +1,1 @@
+var laura = "mui mui";
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    jade = require('gulp-jade'),
+    clean = require('gulp-clean'),
+    paths = {
+        dist: 'dist/',
+        templates: 'app/templates/**/*.jade',
+        js: 'app/js/**/*.js',
+        jsDist: 'dist/js/',
+    };
+
+gulp.task('jade', function() {
+    gulp.src(paths.templates)
+        .pipe(jade())
+        .pipe(gulp.dest(paths.dist))
+});
+
+gulp.task('js', function() {
+    gulp.src(paths.js)
+        .pipe(gulp.dest(paths.jsDist))
+});
+
+gulp.task('clean', function() {
+    gulp.src(['dist/*.html', paths.jsDist])
+        .pipe(clean({ read: false }))
+});
+
+gulp.task('deploy', ['jade', 'js']);
+
+gulp.task('default', ['deploy'], function() {
+    gulp.watch(paths.templates, ['jade']);
+    gulp.watch(paths.js, ['js']);
+});
